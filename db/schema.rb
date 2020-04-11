dc5ba2508a1ec6ee3cb7b5d53d0a284f90126023
Vlad Hoi <hoivladyslav@yahoo.com>
2020-04-11 21:59:14 +0300

    Switched to PostgreSQL.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_09_203814) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
