c7b8a3011f448e2fddc91e28ae0d6c57481173f2
Vlad Hoi <hoivladyslav@yahoo.com>
2020-05-03 13:26:26 +0300

    Fixed styles

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
 class Order < ApplicationRecord
-    enum pay_type: {
-        "Check" => 0,
-        "Credit card" => 1,
-        "Purchase order" => 2
-        }
-    has_many :line_items, dependent: :destroy
-    validates :name, :address, :email, presence: true
-    validates :pay_type, inclusion: pay_types.keys
-    
-        
-    def add_line_items_from_cart(cart)
-        cart.line_items.each do |item|
-            item.cart_id = nil
-            line_items << item
-        end
+  enum pay_type: {
+      "Check" => 0,
+      "Credit card" => 1,
+      "Purchase order" => 2
+  }
+
+  has_many :line_items, dependent: :destroy
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
     end
+  end
 end
